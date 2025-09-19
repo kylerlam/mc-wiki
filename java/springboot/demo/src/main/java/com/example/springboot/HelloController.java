6f77e745fa7d06ca62890f7fe3e8c9e10be2eb9f
monsterchick <kwanho.app@gmail.com>
2025-09-19 23:05:15 +0800

    adds the second router

--- java/springboot/demo/src/main/java/com/example/springboot/HelloController.java
+++ java/springboot/demo/src/main/java/com/example/springboot/HelloController.java
@@ -8,9 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    // 路由根路径
     @GetMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+
+    // 路由 home 路径
+    @GetMapping("/home")
+    public String home() {
+        return "This is home page.!";
     }
 
 }
